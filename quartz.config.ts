e96d4569938dd1e6eeecd6139d6f0605019499b5
eliana <abstractionintheabyss@gmail.com>
2026-06-30 10:54:24 -0400

    Update dark mode light color in quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -42,7 +42,7 @@ const config: QuartzConfig = {
           textHighlight: "#fff23688",
         },
         darkMode: {
-          light: "#00673E",
+          light: "#3C4044",
           lightgray: "#393639",
           gray: "#646464",
           darkgray: "#d4d4d4",
